1707aeaf0ca4638d458bfa36b2e57941bc20a871
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-30 23:58:30 +0600

    Implement single product details data on server

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ app.use(cors());
 
 const products=require('./Data/products.json')
 
-// app.get('/', (req, res)=>{
-
+// app.get('/', (req,res)=>{
 //     res.send(products);
 // })
 
 app.get('/home', (req, res)=>{
 
-    res.send(products.filter(p=>p.mostSell>200));
+    const sort=products.filter(p=>p.mostSell>300);
+    res.send(sort);
 })
 
+app.get('/products', (req, res)=>{
+    res.send(products);
+})
+app.get('/products/:id', (req, res)=>{
+    const params=parseInt(req.params.id);
+    console.log(params)
+    const pId=products.find(p=>parseInt(p._id)==params);
+    console.log(pId)
+    res.send(pId);
+})
 app.listen(port, ()=>console.log(`App running on port ${port}`))
