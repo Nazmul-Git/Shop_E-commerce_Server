9209af996812e6dfcf25e269948d90f6df568513
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-28 13:31:00 +0600

    Install mongodb and implement connections.

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 const express= require('express');
 const app= express();
 const cors=require('cors')
-const port=5000;
+const port=process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
 
 const products=require('./Data/products.json')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 // app.get('/', (req,res)=>{
 //     res.send(products);
 // })
+
+// nazmulhasan16021998
+// TwxOcRlqKEtIiCY6
+
+console.log(process.env.DB_USER)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u4virq8.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    fetch()
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/home', (req, res)=>{
 
