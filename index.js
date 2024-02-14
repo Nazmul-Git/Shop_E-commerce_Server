da30bd2d2e2bc2b0436f69030f35c3d6a7a66027
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-14 15:48:16 +0600

    Update vercel.json file.

--- index.js
+++ index.js
@@ -5,48 +5,48 @@ const port=process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
-
 const products=require('./Data/products.json')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
 
-// app.get('/', (req,res)=>{
-//     res.send(products);
-// })
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config()
 
-// nazmulhasan16021998
-// TwxOcRlqKEtIiCY6
+// console.log(process.env.DB_USER)
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u4virq8.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(process.env.DB_USER)
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u4virq8.mongodb.net/?retryWrites=true&w=majority`;
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     const database = client.db("insertDB");
+//     const haiku = database.collection("haiku");
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-    fetch()
+//     app.post('/products/:id',async(req,res)=>{
+//       const user=req.body;
+//       console.log(user)
+//       const result=await haiku.insertOne(user);
+//       res.send(result);
+//     })
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 app.get('/home', (req, res)=>{
@@ -61,7 +61,7 @@ app.get('/products', (req, res)=>{
 app.get('/products/:id', (req, res)=>{
     const params=parseInt(req.params.id);
     console.log(params)
-    const pId=products.find(p=>parseInt(p._id)==params);
+    const pId=products.find(p=>parseInt(p._id)===params);
     console.log(pId)
     res.send(pId);
 })
